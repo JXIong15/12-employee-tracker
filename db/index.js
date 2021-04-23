ed0a4a9debbed388085fde1297b44f33a3e79728
Pikachu <jouxiong15@gmail.com>
2021-04-22 21:59:03 -0500

    test server

--- db/index.js
+++ db/index.js
@@ -1,7 +1,13 @@
-const connections = require('./connection');
+const connection = require('./connection');
 
 class DB {
     constructor(connection) {
         this.connection = connection;
+    }
+
+    selectAllEmployees() {
+    	return this.connection.query(
+    		"SELECT * from employee"
+    	)
     }
 }
