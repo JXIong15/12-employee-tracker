9631c38650c25e696990cf78acbedea865d2acaf
Pikachu <jouxiong15@gmail.com>
2021-04-23 23:02:29 -0500

    got menu to display appropriate table

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const db = require("./db")
 require("console.table")
-// const inquire = require("inquirer");
+const inquire = require("inquirer");
+const { connection } = require("./db");
 
 console.log("\n-----------------\nEmployee Manager\n-----------------\n");
 
@@ -9,17 +10,18 @@ function start() {
         type: "list",
         name: "start",
         message: "What would you like to do?",
-        choices: ['View All Employees', 'View All Employees By Department', 'View All Employees By Manager', 'Add Employee', 'Remove Employee', 'Update Employee Role', 'Update Employee Manager']
+        choices: ['View All Employees', 'View All Employees By Department', 'View All Employees By Manager', 'Add Employee', 'Remove Employee', 'Update Employee Role', 'Update Employee Manager', 'Quit']
     })
     .then((response) => {
-        switch (response.role) {
-            case "View All Employees": return genEmpView();
+        switch (response.start) {
+            case 'View All Employees': return genEmpView();
             case "View All Employees By Department": return depEmpView();
             case "View All Employees By Manager": return manEmpView();
             case "Add Employee": return addEmp();
             case "Remove Employee": return remEmp();
             case "Update Employee Role": return updateEmpRole();
             case "Update Employee Manager": return updateEmpMan();
+            case "Quit": connection.end();
         }
     })
 }
@@ -27,36 +29,37 @@ function start() {
 async function genEmpView() {
     let allEmployees = await db.selectAllEmployees();
     console.table(allEmployees);
+    start();
 }
 
 async function depEmpView() {
-    console.log("view dep emp")
+    console.log("view dep emp");
+    start();
 }
 
 async function manEmpView() {
-    console.log("view manager emp")
+    console.log("view manager emp");
+    start();
 }
 
 function addEmp() {
-    console.log("add emp")
+    console.log("add emp");
+    start();
 }
 
 function remEmp() {
-    console.log("rem emp")
+    console.log("rem emp");
+    start();
 }
 
 function updateEmpRole() {
-    console.log("update emp role")
+    console.log("update emp role");
+    start();
 }
 
 function updateEmpMan() {
-    console.log("update emp manager")
+    console.log("update emp manager");
+    start();
 }
 
-
-async function viewEmployees() {
-    
-}
-
-// start();
-genEmpView();
+start();
